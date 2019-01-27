82dbe45f7c5b43403b8123b8c4e6cc56b745e656
Woodmanan <mwmccoy@andrew.cmu.edu>
2019-01-26 20:34:01 -0500

    Added Random Furniture Spawning

--- HIWTHI/Assets/TrapController.cs
+++ HIWTHI/Assets/TrapController.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CouchTrap : MonoBehaviour
+public class TrapController : MonoBehaviour
 {
     [SerializeField]
     private int dmg;
+    public bool isLethal;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("Player") || collision.gameObject.tag.Equals("Enemy"))
+        if (isLethal)
         {
-            if (collision.gameObject.tag.Equals("Player"))
+            if (collision.gameObject.tag.Equals("Player") || collision.gameObject.tag.Equals("Enemy"))
             {
-                collision.gameObject.GetComponent<PlayerController>().takeDamage(dmg);
+                if (collision.gameObject.tag.Equals("Player"))
+                {
+                    collision.gameObject.GetComponent<PlayerController>().takeDamage(dmg);
+                }
             }
         }
     }
---
+++ HIWTHI/Assets/TrapSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private int numChoices;
+    private int choice;
+    private GameObject selected;
+    private Object[] choices;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        choices = Resources.LoadAll("Prefabs/Traps", typeof(Object));
+        selected = (GameObject)choices[choice];
+        choice = 0;
+
+        for (int i = 0; i < numChoices; i++)
+        {
+            choice = Random.Range(0, transform.childCount);
+            Transform kid = transform.GetChild(choice);
+            
+            GameObject toSpawn = (GameObject)choices[Random.Range(0, choices.Length)];
+            print("Spawning " + toSpawn.name + " to " + kid.position);
+            GameObject spawned = Instantiate(toSpawn, kid.position, kid.rotation);
+            spawned.GetComponent<TrapController>().isLethal = false;
+            Destroy(kid.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
