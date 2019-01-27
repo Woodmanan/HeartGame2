342449a97fe7addeed657cf52aa308b514051884
kdulla <43546681+kdulla@users.noreply.github.com>
2019-01-27 12:53:26 -0500

    Add files via upload

---
+++ HIWTHI/Assets/fadein.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadein : MonoBehaviour
+{
+    bool condition = true;
+    bool condition2 = true;
+    float curr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curr = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > curr + 2 && condition2)
+        {
+            condition = false;
+            condition2 = false;
+        }
+        Color32 current = GetComponent<SpriteRenderer>().color;
+        int alpha = current.a + 1;
+        if (!condition)
+        {
+            GetComponent<SpriteRenderer>().color = new Color32(current.r, current.g, current.b, (byte)alpha);
+        }
+        if (alpha >= 255)
+        {
+            condition = true;
+        }
+    }
+}
---
+++ HIWTHI/Assets/scrollin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scrollin : MonoBehaviour
+{
+
+    //public AudioClip sound;
+
+    string message;
+    public bool cond = false;
+    public float curr = -4.0f;
+    private int count = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message = GetComponent<UnityEngine.UI.Text>().text;
+        GetComponent<UnityEngine.UI.Text>().text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(cond);
+        if (!cond)
+        {
+            GetComponent<UnityEngine.UI.Text>().text += message.ToCharArray()[count];
+            cond = true;
+            curr = Time.time;
+            count++;
+        }
+        else
+        {
+            if (Time.time - curr > 0.05f && count < message.ToCharArray().Length)
+            {
+                cond = false;
+            }
+        }
+        if (Time.time - curr > 1 && count >= message.ToCharArray().Length)
+        {
+            //add scene transition
+        }
+    }
+}
